bb1c0e97ca990039d7b530843d908a79dd3b885b
timehzy <yiliforever@qq.com>
2022-06-24 18:30:28 +0800

    update

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,25 @@
 import Vapor
+import Foundation
+import Fluent
+import FluentPostgresDriver
+
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.routes.defaultMaxBodySize = "500kb"
+    app.routes.caseInsensitive = true
+    
+    // 自定义json编码
+    let encoder = JSONEncoder()
+    encoder.dateEncodingStrategy = .secondsSince1970
+    ContentConfiguration.global.use(encoder: encoder, for: .json)
+    
     // register routes
     try routes(app)
+
+    // db
+    app.databases.use(.postgres(hostname: "localhost", username: "vapor", password: "vapor", database: "vapor"), as: .psql)
+
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,42 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    app.get("hello", ":name") { req -> String in
+        let name = req.parameters.get("name") ?? "no params"
+        return "hello, \(name)!!"
+    }
+    
+    // 针对个别路径制定body size限制
+    app.on(.POST, "xxx", body: .collect(maxSize: "1mb")) { req in
+        HTTPStatus.ok
+    }
+    
+    // 针对上传文件，使用流来接收body
+    app.on(.POST, "upload", body: .stream) { req -> String in
+//        在使用流的情况下，req.body.data 的值为nil，必须使用下面这种方式接收数据
+//        req.body.drain { streamResult in
+//            switch streamResult {
+//            case .buffer(let byteBuffer):
+//                print(byteBuffer)
+//            case .error(let error):
+//                print(error)
+//            case .end:
+//                print("upload end")
+//            }
+//            return .
+//        }
+        return "upload"
+    }
+    
+    // 使用组来组织一组由共同前缀构成的路由
+    app.group("user") { user in
+        user.get { req in
+            req.redirect(to: "hello", type: .normal)
+        }
+        user.post { req in
+            "user post"
+        }
+    }
+
 }
